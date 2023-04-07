2f1397a1ffa4a648efda4c62ba5c66caf9d32843
Ellie Shivers <elliemshivers@gmail.com>
2023-04-07 10:55:07 -0400

    0.0.2

---
+++ lib/ngx-fhir-validator.module.d.ts
@@ -0,0 +1,31 @@
+import * as i0 from "@angular/core";
+import * as i1 from "./components/ngx-fhir-validator.component";
+import * as i2 from "@angular/common";
+import * as i3 from "@angular/platform-browser";
+import * as i4 from "@angular/platform-browser/animations";
+import * as i5 from "@angular/material/form-field";
+import * as i6 from "@angular/material/input";
+import * as i7 from "@angular/material/button";
+import * as i8 from "@angular/forms";
+import * as i9 from "@angular/material/icon";
+import * as i10 from "@angular/material/sidenav";
+import * as i11 from "@angular/material/toolbar";
+import * as i12 from "@angular/material/table";
+import * as i13 from "@angular/material/paginator";
+import * as i14 from "@angular/material/progress-spinner";
+import * as i15 from "@angular/material/sort";
+import * as i16 from "@angular/common/http";
+import * as i17 from "@angular/material/radio";
+import * as i18 from "@angular/material/card";
+import * as i19 from "@angular/material/snack-bar";
+import * as i20 from "@angular/material/select";
+import * as i21 from "@angular/material/button-toggle";
+import * as i22 from "@angular/material/chips";
+import * as i23 from "@angular/material/checkbox";
+import * as i24 from "@angular/material/tooltip";
+import * as i25 from "@angular/material/divider";
+export declare class NgxFhirValidatorModule {
+    static ɵfac: i0.ɵɵFactoryDeclaration<NgxFhirValidatorModule, never>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<NgxFhirValidatorModule, [typeof i1.NgxFhirValidatorComponent], [typeof i2.CommonModule, typeof i3.BrowserModule, typeof i4.BrowserAnimationsModule, typeof i5.MatFormFieldModule, typeof i6.MatInputModule, typeof i7.MatButtonModule, typeof i8.ReactiveFormsModule, typeof i9.MatIconModule, typeof i10.MatSidenavModule, typeof i11.MatToolbarModule, typeof i12.MatTableModule, typeof i13.MatPaginatorModule, typeof i14.MatProgressSpinnerModule, typeof i15.MatSortModule, typeof i16.HttpClientModule, typeof i17.MatRadioModule, typeof i8.FormsModule, typeof i18.MatCardModule, typeof i19.MatSnackBarModule, typeof i20.MatSelectModule, typeof i21.MatButtonToggleModule, typeof i22.MatChipsModule, typeof i23.MatCheckboxModule, typeof i24.MatTooltipModule, typeof i25.MatDividerModule], [typeof i1.NgxFhirValidatorComponent]>;
+    static ɵinj: i0.ɵɵInjectorDeclaration<NgxFhirValidatorModule>;
+}
